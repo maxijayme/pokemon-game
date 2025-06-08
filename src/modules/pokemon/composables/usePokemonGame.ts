17adf9ab71f2fda4d27d8046ffb68bbbf8edad2c
maxijayme <maxijayme@gmail.com>
2025-06-08 20:11:16 +0200

    suffle pokemons, get pokemons options, isLoading controller

--- src/modules/pokemon/composables/usePokemonGame.ts
+++ src/modules/pokemon/composables/usePokemonGame.ts
@@ -1,26 +1,55 @@
-import { onMounted, ref } from "vue"
-import { GameStatus, type PokemonListResponse } from "../interfaces"
+import { computed, onMounted, ref } from "vue"
+import { GameStatus, type PokemonListResponse, type Pokemon } from "../interfaces"
 import { pokemonApi } from "@/api/pokemonApi"
 
 export const usePokemonGame = () => {
 
-  const getPokemonList = async() => {
+  const gameStatus = ref<GameStatus>(GameStatus.Playing)
+
+  const pokemons = ref<Pokemon[]>([])
+  const pokemonOptions = ref<Pokemon[]>([])
+
+  const isLoading = computed(() => pokemons.value.length === 0)
+
+  const getPokemons = async(): Promise<Pokemon[]> => {
 
     const response = await pokemonApi.get<PokemonListResponse>('?limit=151')
 
-    return response.data
+    const pokemonList = response.data.results.map(pokemon => {
+      const urlParts = pokemon.url.split('/')
+      const id = urlParts.at(-2) ?? '0'
+      return {
+        id: parseInt(id, 10),
+        name: pokemon.name
+      }
+    })
+
+    const suffledPokemonList = pokemonList.sort(() => Math.random() - 0.5)
+    return suffledPokemonList
   }
 
-  const gameStatus = ref<GameStatus>(GameStatus.Playing)
+  const getNextOPtions = ( howMany: number = 4) => {
+    gameStatus.value = GameStatus.Playing
+    pokemonOptions.value = pokemons.value.slice(0, howMany)
+    pokemons.value = pokemons.value.slice(howMany)
+  }
+
 
   onMounted(
     async () => {
-      const pokemonList = await getPokemonList()
-      console.log(pokemonList)
+      await new Promise( resolve => setTimeout(resolve, 1000) )
+      pokemons.value = await getPokemons()
+      getNextOPtions()
     }
   )
 
   return {
-    gameStatus
+    gameStatus,
+    isLoading,
+    pokemonOptions,
+
+    //Methods
+    getNextOPtions,
+    getPokemons,
   }
 }
--- src/modules/pokemon/pages/PokemonGame.vue
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -3,16 +3,16 @@ import PokemonOptions from '../components/PokemonOptions.vue';
 import PokemonPicture from '../components/PokemonPicture.vue';
 import { usePokemonGame } from '../composables/usePokemonGame';
 
-const { gameStatus } = usePokemonGame()
+const { gameStatus, isLoading } = usePokemonGame()
 
 </script>
 
 <template>
-  <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+  <section v-if="isLoading" class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="text-3xl font-bold mb-4">Please wait</h1>
     <h3 class="animate-pulse">Loading Pokémon...</h3>
   </section>
-  <section class="flex flex-col justify-center items-center w-screen h-screen">
+  <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="text-3xl font-bold mb-4">Who is this Pokémon?</h1>
     <!-- Pokemon picture -->
     <PokemonPicture />
