04555c3ff9df949a989e3f447d47cc3c409eef4c
maxijayme <maxijayme@gmail.com>
2025-08-25 20:25:35 +0200

    confetti mock

--- tests/modules/pokemon/composables/usePokemongame.test.ts
+++ tests/modules/pokemon/composables/usePokemongame.test.ts
@@ -5,12 +5,17 @@ import { usePokemonGame } from '@pokemon/composables/usePokemonGame'
 import { pokemonApi } from '@/modules/pokemon/api/pokemonApi'
 import { pokemonListFake } from '../../../data/fake-pokemons'
 import { flushPromises } from '@vue/test-utils'
+import confetti from 'canvas-confetti'
 
 const mockPokemonApi = new MockAdapter(pokemonApi)
 
 mockPokemonApi.onGet('/?limit=151').reply(200, {
   results: pokemonListFake,
 })
+
+vi.mock('canvas-confetti', () => ({
+  default: vi.fn(),
+}))
 
 describe('usePokemonGame', () => {
   test('should initialize with correct deafult pokemon values', async () => {
@@ -52,5 +57,37 @@ describe('usePokemonGame', () => {
     results.getNextRound()
     const nextPokemonOptions = results.pokemonOptions.value
     expect(firstPokemonOptions[0]).not.toMatchObject(nextPokemonOptions[0])
+  })
+
+  test('should handle correctly incorrect answer', async () => {
+    const [results] = withSetup(usePokemonGame)
+    await flushPromises()
+
+    const { gameStatus, checkSelection, randomPokemon } = results
+    expect(gameStatus.value).toBe(GameStatus.Playing)
+    checkSelection(randomPokemon.value.id + 1)
+    expect(gameStatus.value).toBe(GameStatus.Lost)
+  })
+
+  test('should handle correctly correct answer', async () => {
+    const [results] = withSetup(usePokemonGame)
+    await flushPromises()
+
+    const { gameStatus, checkSelection, randomPokemon } = results
+    expect(gameStatus.value).toBe(GameStatus.Playing)
+    checkSelection(randomPokemon.value.id)
+    expect(confetti).toBeCalled()
+    expect(confetti).toBeCalledWith({
+      spread: 360,
+      ticks: 50,
+      gravity: 0,
+      decay: 0.94,
+      startVelocity: 30,
+      colors: ['FFE400', 'FFBD00', 'E89400', 'FFCA6C', 'FDFFB8'],
+      particleCount: 40,
+      scalar: 1.2,
+      shapes: ['star'],
+    })
+    expect(gameStatus.value).toBe(GameStatus.Won)
   })
 })
